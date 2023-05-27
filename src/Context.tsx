c4cacec3207f24dd24568d7774476569d4d5f511
Derek Wan <d.wan@icloud.com>
2023-05-27 15:20:07 +0800

    Read photos (#5)

--- src/Context.tsx
+++ src/Context.tsx
@@ -1,22 +1,23 @@
-import { useState, createContext, Dispatch, SetStateAction } from "react";
+import { readData, Photo } from "./data";
+import { useState, createContext } from "react";
 
 type ContextType = {
-  value: string;
-  setValue?: Dispatch<SetStateAction<string>>;
+  photos: Photo[];
 };
-const defaultState = { value: "" };
+const defaultState = { value: "", photos: await readData() };
 export const Context = createContext<ContextType>(defaultState);
 
 type Props = {
   children: React.ReactElement;
+  initPhotos?: Photo[];
 };
 
 export function ContextProvider(props: Props) {
-  const [value, setValue] = useState<string>("");
+  const [photos, _] = useState<Photo[]>(
+    props.initPhotos ?? defaultState.photos
+  );
 
   return (
-    <Context.Provider value={{ value, setValue }}>
-      {props.children}
-    </Context.Provider>
+    <Context.Provider value={{ photos }}>{props.children}</Context.Provider>
   );
 }
