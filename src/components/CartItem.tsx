96a8cf959bd35eddbfee6850571380e578d3cba1
Derek Wan <d.wan@icloud.com>
2023-05-27 17:27:50 +0800

    Can delete items (#16)

--- src/components/CartItem.tsx
+++ src/components/CartItem.tsx
@@ -1,13 +1,21 @@
+import { Context } from "../Context";
 import { Photo } from "../data";
+import { useContext } from "react";
 
 type Props = {
   photo: Photo;
 };
 
 export default function CartItem(props: Props) {
+  const context = useContext(Context);
+
+  const handleClick = () => {
+    context.toggleCart(props.photo);
+  };
+
   return (
     <div className="cart-item">
-      <i className="ri-delete-bin-line"></i>
+      <i className="ri-delete-bin-line" onClick={handleClick}></i>
       <img src={props.photo.url} alt="" width="130px" />
       <p>$5.99</p>
     </div>
