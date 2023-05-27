dfe3de9b289c59c1595ec928a3dce4fa0e104fde
Derek Wan <d.wan@icloud.com>
2023-05-27 14:49:37 +0800

    Read photos

--- src/pages/Photos.tsx
+++ src/pages/Photos.tsx
@@ -1,7 +1,22 @@
+import { Context } from "../Context";
+import { Photo } from "../data";
+import { useContext } from "react";
+
 export default function Photos() {
+  const context = useContext(Context);
+  const render = (photo: Photo) => {
+    return (
+      <div>
+        <img src={photo.url} alt="" />
+        <p> {photo.isFavorite ? "isFav" : "isNotFav"} </p>
+      </div>
+    );
+  };
+  const photos = context.photos.map(render);
   return (
     <main className="photos">
       <h1>Images go here</h1>
+      {photos}
     </main>
   );
 }
