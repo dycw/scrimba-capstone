ced16f873c3ff635ff81dbacaba862465c0dc80b
Derek Wan <d.wan@icloud.com>
2023-05-27 17:15:59 +0800

    Header will show cart filled, and add `CartItem`

--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -1,7 +1,22 @@
+import { Context } from "../Context";
+import CartItem from "../components/CartItem";
+import { Photo } from "../data";
+import { useContext } from "react";
+
 export default function Cart() {
+  const context = useContext(Context);
+
+  const render = (photo: Photo) => <CartItem key={photo.id} photo={photo} />;
+  const cart = context.cart.map(render);
+
   return (
     <main className="cart-page">
       <h1>Check out</h1>
+      {cart}
+      <p className="total-cost">Total: </p>
+      <div className="order-button">
+        <button>Place Order</button>
+      </div>
     </main>
   );
 }
