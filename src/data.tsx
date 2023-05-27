7d7fab95136799d82345d3c1cc1b1c1b00941dd6
Derek Wan <d.wan@icloud.com>
2023-05-27 15:45:12 +0800

    Fetch photos using `useEffect` (#6)

--- src/data.tsx
+++ src/data.tsx
@@ -2,10 +2,7 @@ type RawPhoto = { url: string; id: string; isFavorite: boolean };
 export type Photo = { url: string; id: number; isFavorite: boolean };
 
 export const readData = async (): Promise<Photo[]> => {
-  const resp = await fetch(
-    // "https://raw.githubusercontent.com/bobziroll/scrimba-react-bootcamp-images/master/images.json",
-    "scrimba-react-bootcamp-images/images.json"
-  );
+  const resp = await fetch("scrimba-react-bootcamp-images/images.json");
   const rawPhotos: RawPhoto[] = await resp.json();
   const convert = (p: RawPhoto): Photo => {
     return { url: p.url, id: parseInt(p.id), isFavorite: p.isFavorite };
